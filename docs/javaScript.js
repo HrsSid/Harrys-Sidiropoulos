f9291b6cc0c52a09c063b16f795837abe217218a
G6H4 <proplopaido@gmail.com>
2023-04-23 18:58:20 +0300

    $fixing the project email sent form

--- docs/javaScript.js
+++ docs/javaScript.js
@@ -5,8 +5,19 @@ function sendEmailProject() {
     let useremail = document.querySelector(".email-input-project").value;
 
     emailjs.send("service_00gco5o","template_nhpcjqn",{
-        from_name: projectName +"\nE-mail: "+ useremail +"\nBudget (in EURO): " + projectbudget,
+        from_name: "Project Name: "+ projectName +"\nE-mail: "+ useremail +"\nBudget (in EURO): " + projectbudget,
         message: projectDetails,
+    });
+    alert("Your project was sent successfully");
+}
+function sendEmail() {
+    let username = document.querySelector(".userName").value
+    let usermessage = document.getElementById('emailSubject').value
+    let useremail = document.querySelector(".email-input-contact").value
+
+    emailjs.send("service_00gco5o","template_nhpcjqn",{
+        from_name: "Name: "+ username +"\n"+ "E-mail: "+ useremail,
+        message: usermessage,
     });
     alert("Your e-mail was sent successfully");
 }
